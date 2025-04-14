5d01dc6abb02777ad746b990e0ca3d898440426a
Nathan-Mrlt <nathan.mrlt@gmail.com>
2025-04-14 14:15:31 +0200

    add user reducer

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { SafeAreaView, StyleSheet } from "react-native";
 import { House, Utensils, Brain, CircleUserRound } from "lucide-react-native";
 
-//imports for tab navigation
+// Imports pour la tab navigation
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./screens/HomeScreen";
@@ -9,7 +9,7 @@ import SearchScreen from "./screens/SearchScreen";
 import QuizScreen from "./screens/QuizScreen";
 import UserScreen from "./screens/UserScreen";
 
-// imports for stack navigation
+// Imports pour la stack navigation
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import EnterScreen from "./screens/EnterScreen";
 import FavoritesScreen from "./screens/FavoritesScreen";
@@ -20,10 +20,17 @@ import RegisterScreen from "./screens/RegisterScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 
+// Imports pour le store redux
+import { Provider, useSelector } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import user from "./reducers/user";
+
+// Configuration de la naviguation
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
+  const user = useSelector((state) => state.user.value);
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
@@ -36,38 +43,59 @@ const TabNavigator = () => {
             return <Brain size={size} color={color} />;
           } else if (route.name === "User") {
             return <CircleUserRound size={size} color={color} />;
+          } else if (route.name === "Enter") {
+            return <CircleUserRound size={size} color={color} />;
           }
         },
         tabBarActiveTintColor: "#6ac46a",
         tabBarInactiveTintColor: "#173e19",
+        tabBarShowLabel: false, // Enlever l'affichage des labels
+        tabBarItemStyle: {
+          paddingTop: 5, // Ajout de padding pour centrer les icônes (le flex n'a pas l'air de fonctionner...)
+        },
         headerShown: false,
       })}
     >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Search" component={SearchScreen} />
       <Tab.Screen name="Quiz" component={QuizScreen} />
-      <Tab.Screen name="User" component={UserScreen} />
+      {user.token === null ? (
+        <Tab.Screen
+          name="Enter"
+          component={EnterScreen}
+          options={{ tabBarStyle: { display: "none" } }}
+        />
+      ) : (
+        <Tab.Screen name="User" component={UserScreen} />
+      )}
     </Tab.Navigator>
   );
 };
 
+// Configuration du store redux
+const store = configureStore({
+  reducer: { user },
+});
+
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Welcome" component={WelcomeScreen} />
-          <Stack.Screen name="TabNavigator" component={TabNavigator} />
-          <Stack.Screen name="Enter" component={EnterScreen} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Register" component={RegisterScreen} />
-          <Stack.Screen name="Favorites" component={FavoritesScreen} />
-          <Stack.Screen name="History" component={HistoryScreen} />
-          <Stack.Screen name="Question" component={QuestionScreen} />
-          <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaView>
+    <Provider store={store}>
+      <SafeAreaView style={styles.container}>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Welcome" component={WelcomeScreen} />
+            <Stack.Screen name="TabNavigator" component={TabNavigator} />
+            <Stack.Screen name="Enter" component={EnterScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="Favorites" component={FavoritesScreen} />
+            <Stack.Screen name="History" component={HistoryScreen} />
+            <Stack.Screen name="Question" component={QuestionScreen} />
+            <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    </Provider>
   );
 }
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text>HomeScreen</Text>
