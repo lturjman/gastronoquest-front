3ad21dc0983441b95a67b0e164fbe9bccd62a6bb
Laura Turjman <contact@lauraturjman.fr>
2025-04-14 11:17:28 +0200

    button with variants

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import Button from "./components/ui-kit/button";
 
 //imports for tab navigation
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import HomeScreen from "./screens/HomeScreen";
-import SearchScreen from "./screens/SearchScreen";
-import QuizScreen from "./screens/QuizScreen";
-import UserScreen from "./screens/UserScreen";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import HomeScreen from "./screens/HomeScreen";
+// import SearchScreen from "./screens/SearchScreen";
+// import QuizScreen from "./screens/QuizScreen";
+// import UserScreen from "./screens/UserScreen";
 
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
+// const Stack = createNativeStackNavigator();
+// const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <SafeAreaView
+        style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+      >
+        <Button title="Container Button" variant="light" />
+        <Button title="Container Button" variant="dark" />
+        <Button title="Container Button" variant="outline" />
+      </SafeAreaView>
     </View>
   );
 }
