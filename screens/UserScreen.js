b6a2bff86de7855139d8f68b3760692ae76e5849
Nathan-Mrlt <nathan.mrlt@gmail.com>
2025-04-16 15:06:55 +0200

    add button to navigate to favoritesScreen on userScreen

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,9 +1,17 @@
 import { StyleSheet, Text, View } from "react-native";
+import CustomButton from "../components/ui-kit/CustomButton";
 
-export default function UserScreen() {
+export default function UserScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>UserScreen</Text>
+      <View>
+        <CustomButton
+          title={"Mes favoris"}
+          onPress={() =>
+            navigation.navigate("User", { screen: "FavoritesScreen" })
+          }
+        />
+      </View>
     </View>
   );
 }
