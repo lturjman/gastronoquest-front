80183b8e86a73628b9cd0c98b23ed813a92b0d23
Nathan-Mrlt <nathan.mrlt@gmail.com>
2025-04-18 12:02:13 +0200

    start to work on restaurantScreen

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,29 +1,33 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
 import { useRoute } from "@react-navigation/native";
-import { ArrowLeft, Heart } from "lucide-react-native";
+import {
+  ArrowLeft,
+  Heart,
+  Leaf,
+  MapPin,
+  UtensilsCrossed,
+  Store,
+  Euro,
+} from "lucide-react-native";
 import { useState } from "react";
 
-export default function RestaurantScreen() {
+const { width } = Dimensions.get("window");
+
+export default function RestaurantScreen({ navigation }) {
+  // State pour le bouton like
   const [liked, setLiked] = useState(false);
+  // State pour l'onglet sélectionné
+  // Soit challenges, soit description
+  const [selectedTab, setSelectedTab] = useState("description");
   // Récupération des données du restaurant
   const route = useRoute();
-
-  /*
-  {
-    name: str, XX
-    desc: str,
-    longDesc: str, XX
-    score: number, XX
-    badges: [str], XX
-    types: [str] XX
-    priceRange: str, XX
-    address: str, XX
-    coordinates: {latitude: number, longitude: number},
-    imageUrl: str, XX
-    websiteUrl: str, XX
-    bookingUrl: str,
-  }
-  */
 
   const {
     name,
@@ -35,23 +39,80 @@ export default function RestaurantScreen() {
     address,
     imageUrl,
     websiteUrl,
-  } = route.params;
+  } = route.params.restaurant;
+
+  // Conversion du score Ecotable en feuilles
+  const leaves = [];
+  for (let i = 0; i < score; i++) {
+    leaves.push(<Leaf key={i} color="#173e19" size={20} />);
+  }
 
   return (
     <View style={styles.container}>
+      {/*Partie haute de la page  avec la présentation du restaurant sans les onglets*/}
       <View style={styles.presentationContainer}>
+        {/* Header */}
         <View style={styles.headerContainer}>
           <View style={styles.headerLeft}>
-            <ArrowLeft color={"black"} size={18} />
-            <Text>{name}</Text>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <ArrowLeft color={"black"} size={23} />
+            </TouchableOpacity>
+            <Text style={{ fontSize: 23, fontWeight: 500 }}>{name}</Text>
           </View>
           <TouchableOpacity
             onPress={() => setLiked(!liked)}
             style={liked ? styles.liked : styles.notLiked}
           >
-            <Heart color={liked ? "#red" : "#fff"} size={25} />
+            <Heart color={liked ? "red" : "black"} size={25} />
           </TouchableOpacity>
         </View>
+        {/*Image du restaurant*/}
+        <Image
+          source={{ uri: imageUrl }}
+          style={styles.image}
+          resizeMode="cover"
+        />
+        {/* Bloc qui contient les métadonnées (prix, types, addresse, etc.) */}
+        <View style={styles.metadataContainer}>
+          <View style={styles.score}>
+            {leaves}
+            <Text style={styles.scoreLabel}>Label Ecotable</Text>
+          </View>
+          <View style={styles.itemPresentationContainer}>
+            <MapPin size={20} color={"black"} />
+            <Text>{address}</Text>
+          </View>
+          <View style={styles.itemPresentationContainer}>
+            <UtensilsCrossed size={20} color={"black"} />
+            <View style={styles.tagContainer}>
+              {badges.map((badge, i) => (
+                <View key={i} style={[styles.tag, styles.badge]}>
+                  <Text style={styles.tagText}>{badge}</Text>
+                </View>
+              ))}
+            </View>
+          </View>
+          <View style={styles.itemPresentationContainer}>
+            <Store size={20} color={"black"} />
+            <View style={styles.tagContainer}>
+              {types.map((badge, i) => (
+                <View key={i} style={[styles.tag, styles.type]}>
+                  <Text style={styles.tagText}>{badge}</Text>
+                </View>
+              ))}
+            </View>
+          </View>
+          <View style={styles.itemPresentationContainer}>
+            <Euro size={20} color={"black"} />
+            <Text>{priceRange}</Text>
+          </View>
+        </View>
+      </View>
+      {/* Barre avec les onglets */}
+      <View>
+        <TouchableOpacity>
+          <Text></Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -63,9 +124,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#F9F9F9",
     alignItems: "center",
     justifyContent: "flex-start",
+    paddingTop: 20,
   },
   presentationContainer: {
     width: "90%",
+    gap: 20,
   },
   headerContainer: {
     flexDirection: "row",
@@ -75,5 +138,48 @@ const styles = StyleSheet.create({
   headerLeft: {
     flexDirection: "row",
     gap: 10,
+    alignItems: "center",
+  },
+  image: {
+    width: width * 0.9,
+    height: 200,
+    borderRadius: 10,
+  },
+  metadataContainer: {
+    gap: 9,
+  },
+  score: {
+    flexDirection: "row",
+  },
+  scoreLabel: {
+    marginLeft: 10,
+  },
+  itemPresentationContainer: {
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+  },
+  tagContainer: {
+    flexDirection: "row",
+    gap: 5,
+    flexWrap: "wrap",
+  },
+  tag: {
+    borderRadius: 20,
+    paddingHorizontal: 11,
+    paddingVertical: 3,
+  },
+  badge: {
+    backgroundColor: "#1C3B1D",
+  },
+  type: {
+    backgroundColor: "#6ac46a",
+  },
+  tagText: {
+    fontSize: 10,
+    color: "#fff",
+  },
+  price: {
+    backgroundColor: "#eee", //gris
   },
 });
