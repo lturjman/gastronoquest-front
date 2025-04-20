e0737ed894d464e122082b658c8e2a00149eddf0
Morgan Dumond <morgan.dumond@outlook.com>
2025-04-20 19:02:47 +0200

    Ajout de la mécanique d'ajout/suppression de favori / redirection vers EnterScreen

--- components/ui-kit/RestaurantCard.js
+++ components/ui-kit/RestaurantCard.js
@@ -2,17 +2,70 @@ import { View, Text, StyleSheet, TouchableOpacity, Platform } from "react-native
 import { Leaf, Heart } from "lucide-react-native";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigation } from "@react-navigation/native";
+import { addFavorite, removeFavorite } from "../../reducers/user";
 
 export default function RestaurantCard({ restaurant }) {
   const navigation = useNavigation();
   const dispatch = useDispatch();
-
   const favorites = useSelector((state) => state.user.value.favorites);
-  const liked = favorites.includes(restaurant._id);
+  const token = useSelector((state) => state.user.value.token);
+  const isFavorite = favorites.includes(restaurant._id);
 
   const leaves = [];
   for (let i = 0; i < restaurant.score; i++) {
     leaves.push(<Leaf key={i} color="#173e19" size={20} />);
+  }
+
+  const handleFavorite = async () => {
+    
+    if (!token) {
+      navigation.navigate('EnterScreen');
+      return;
+    }
+
+    if (isFavorite) {
+      try {
+        const response = await fetch(
+          `${process.env.EXPO_PUBLIC_BACKEND_URL}/favorites`,
+          {
+            method: "DELETE",
+            headers: { "Content-Type": "application/json", authorization: token },
+            body: JSON.stringify({
+              restaurantId: restaurant._id,
+            }),
+          }
+        );
+        const data = await response.json();
+        if (data.result) {
+          dispatch(removeFavorite(restaurant));
+        } else {
+          throw new Error("Failed to remove favorite");
+        }
+      } catch (error) {
+        console.error("Error removing favorite:", error);
+      }
+    } else {
+      try {
+        const response = await fetch(
+          `${process.env.EXPO_PUBLIC_BACKEND_URL}/favorites`,
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json", authorization: token },
+            body: JSON.stringify({
+              restaurantId: restaurant._id,
+            }),
+          }
+        );
+        const data = await response.json();
+        if (data.result) {
+          dispatch(addFavorite(restaurant));
+        } else {
+          throw new Error("Failed to add favorite");
+        }
+      } catch (error) {
+        console.error("Error adding favorite:", error);
+      }
+    }
   }
 
   return (
@@ -24,8 +77,8 @@ export default function RestaurantCard({ restaurant }) {
         <TouchableOpacity style={styles.nameContainer} onPress={() => navigation.navigate("RestaurantScreen", { restaurant })}>
           <Text style={styles.name}>{restaurant.name}</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.favorite} onPress={() => setLiked(!liked)}>
-          <Heart color={liked ? "#e53935" : "#173e19"} size={25} style={{ marginTop: 5 }} />
+        <TouchableOpacity style={styles.favorite} onPress={() => handleFavorite()}>
+          <Heart color={isFavorite ? "#e53935" : "#173e19"} size={25} style={{ marginTop: 5 }} />
         </TouchableOpacity>
       </View>
 
@@ -50,15 +103,12 @@ export default function RestaurantCard({ restaurant }) {
           </View>
         ))}
         {/* Types */}
-        { restaurant.types.map((badge, i) => (
+        { restaurant.types.map((type, i) => (
           <View key={i} style={[styles.tag, styles.type]}>
-            <Text style={styles.tagText}>{badge}</Text>
+            <Text style={styles.tagText}>{type}</Text>
           </View>
         ))}
       </View>
-
-
-
 
       </View>
     </View>
@@ -134,152 +184,3 @@ const styles = StyleSheet.create({
     backgroundColor: "#eee", //gris
   }
 });
-
-
-
-/*
-import { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Platform } from "react-native";
-import { Leaf, Heart } from "lucide-react-native";
-import { useSelector, useDispatch } from "react-redux";
-import { useNavigation } from "@react-navigation/native";
-
-export default function RestaurantCard({ restaurant }) {
-  const navigation = useNavigation();
-  const dispatch = useDispatch();
-
-  const token = useSelector((state) => state.user.value.token);
-  const favorites = useSelector((state) => state.user.value.favorites);
-
-  const [liked, setLiked] = useState(favorites.includes(restaurant._id));
-
-
-  const leaves = [];
-  for (let i = 0; i < restaurant.score; i++) {
-    leaves.push(<Leaf key={i} color="#173e19" size={20} />);
-  }
-
-  return (
-    <View style={styles.card}>
-      <View style={styles.content}>
-
-      <View style={styles.header}>
-        <TouchableOpacity style={styles.nameContainer} onPress={() => navigation.navigate("RestaurantScreen", { restaurant })}>
-          <Text style={styles.name}>{restaurant.name}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.favorite} onPress={() => setLiked(!liked)}>
-          <Heart color={liked ? "#e53935" : "#173e19"} size={25} style={{ marginTop: 5 }} />
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.row}>
-        <View style={styles.score}>
-          { leaves }
-        </View>
-        <Text> - </Text>           
-        <Text style={styles.address}>{restaurant.priceRange}</Text>
-      </View>
-
-      <View style={styles.tagContainer}>
-          <View style={[styles.tag, styles.price]}>
-            <Text style={styles.priceText}>{restaurant.address}</Text>
-          </View>
-      </View>
-
-      <View style={styles.tagContainer}>
-        { restaurant.badges.map((badge, i) => (
-          <View key={i} style={[styles.tag, styles.badge]}>
-            <Text style={styles.tagText}>{badge}</Text>
-          </View>
-        ))}
-      </View>
-
-      <View style={styles.tagContainer}>
-      { restaurant.types.map((badge, i) => (
-          <View key={i} style={[styles.tag, styles.type]}>
-            <Text style={styles.tagText}>{badge}</Text>
-          </View>
-        ))}
-      </View>
-
-      </View>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  card: {
-    zIndex: 1,
-    backgroundColor: "#fff",
-    borderRadius: 10,
-    shadowColor: "#000",
-    shadowOpacity: 0.1,
-    shadowRadius: 10,
-    elevation: 3,
-    padding: 20,
-    fontFamily: Platform.select({
-      ios: "Helvetica Neue",
-      android: "Roboto",
-      default: "System",
-    }),
-  },
-  content: {
-    gap: 8,
-  },
-  header: {
-    maxWidth: '100%',
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "start",
-    gap: 15,
-  },
-  nameContainer: {
-    flexShrink: 1,
-  },
-  name: {
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: -5
-  },
-  score: {
-    flexDirection: "row",
-  },
-  address: {
-    fontSize: 15,
-    color: "#333",
-  },
-  tagContainer: {
-    flexDirection: "row",
-    gap: 5,
-    flexWrap: 'wrap'
-  },
-  tag: {
-    borderRadius: 20,
-    paddingHorizontal: 11,
-    paddingVertical: 3,
-  },
-  badge: {
-    backgroundColor: "#1C3B1D",
-  },
-  type: {
-    backgroundColor: "#6ac46a",
-  },
-  tagText: {
-    fontSize: 14,
-    color: "#fff",
-    fontWeight: "600",
-  },
-  price: {
-    backgroundColor: "#eee", //gris
-  },
-  priceText: {
-    fontSize: 14,
-    color: "#333",
-    fontWeight: "600",
-  },
-});
-*/
