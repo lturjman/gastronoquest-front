0f56e3875df9a6acf95d10e80ed70590f967680f
Morgan Dumond <morgan.dumond@outlook.com>
2025-04-22 17:18:51 +0200

    Implémentation du reducer guest et création du service handleSubmitQuiz

---
+++ services/handleSubmitQuiz.js
@@ -0,0 +1,47 @@
+import { saveGuestQuiz } from "../reducers/guest";
+
+export const handleSubmitQuiz = async (dispatch, navigation, token, quizId, score) => {
+
+  // Si 70% de bonnes réponses, la série est réussie
+  const passed = score >= 7;
+
+  // Si l'utilisateur n'est pas connecté
+  if (token === null) {
+    console.log("Utilisateur pas connecté");
+    // Sauvegarde du résultat dans le reducer guest
+    const quizResult = {
+      quizId,
+      score,
+      passed,
+      passedAt: Date.now()
+    };
+    dispatch(saveGuestQuiz(quizResult));
+    // Redirection vers la page EnterScreen
+    return navigation.navigate("Enter");
+  }
+
+  // Si l'utilisateur est connecté, envoi des résultats au backend
+  try {
+    const response = await fetch(
+      `${process.env.EXPO_PUBLIC_BACKEND_URL}/quizResults`,
+      {
+        method: "PUT",
+        headers: { "Content-Type": "application/json", authorization: token },
+        body: JSON.stringify({ quizId, score, passed }),
+      }
+    );
+
+    const data = await response.json();
+    
+    // Si l'ajout s'est bien passé
+    if (data.result) {
+      // Redirection vers la page QuizScreen
+      navigation.navigate("Quiz", { screen: "QuizScreen" });
+    } else {
+      throw new Error("Failed to save quiz result");
+    }
+
+  } catch (error) {
+    console.error(error);
+  }
+};
