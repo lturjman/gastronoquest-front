98192b27ffc50339647e8b646a035f034d4caf03
Nathan-Mrlt <nathan.mrlt@gmail.com>
2025-04-23 12:24:08 +0200

    split homeScreen in components

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,12 +9,11 @@ import {
   ImageBackground,
 } from "react-native";
 import { SwiperFlatList } from "react-native-swiper-flatlist";
-import { Animated } from "react-native";
 import CustomButton from "../components/ui-kit/CustomButton";
 import { useSelector } from "react-redux";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import { useEffect, useRef } from "react";
+import HomeCo2Container from "../components/HomeCo2Container";
 
 const news = [
   {
@@ -34,159 +33,8 @@ const news = [
 
 const { width, height } = Dimensions.get("window");
 
-const levelIcons = {
-  "Jeune pousse": "🌱",
-  "Petit arbuste": "🪴",
-  "Arbre fruitier": "🍎",
-  "Grand arbre": "🌴",
-  "Chêne centenaire": "🌳",
-};
-
-// Fonction pour récupérer le niveau selon les kg de CO2 économisés
-const getUserLevel = (co2) => {
-  if (co2 > 100) return "Chêne centenaire";
-  if (co2 >= 75) return "Grand arbre";
-  if (co2 >= 50) return "Arbre fruitier";
-  if (co2 >= 20) return "Petit arbuste";
-  return "Jeune pousse";
-};
-
-// Niveaux + seuils pour progression
-const levelThresholds = [
-  { level: "jeune pousse", icon: "🌱", co2: 10 },
-  { level: "Petit arbuste", icon: "🪴", co2: 20 },
-  { level: "Arbre fruitier", icon: "🍎", co2: 50 },
-  { level: "Grand arbre", icon: "🌴", co2: 75 },
-  { level: "Chêne centenaire", icon: "🌳", co2: 100 },
-];
-
-// Fonction pour trouver le prochain niveau et combien il reste pour l'atteindre
-const getNextLevelInfo = (co2) => {
-  for (let i = 0; i < levelThresholds.length; i++) {
-    // On parcourt tous les paliers définis dans levelThresholds
-    if (co2 < levelThresholds[i].co2) {
-      // Si le CO2 actuel est inférieur à celui du palier courant
-      return {
-        nextLevel: levelThresholds[i].level, // On retourne un objet contenant :
-        icon: levelThresholds[i].icon,
-        remaining: levelThresholds[i].co2 - co2, // le nombre de kg restants pour y arriver
-      };
-    }
-  }
-};
-
-// Calcule le pourcentage d'avancement vers le prochain niveau
-const getProgressPercentage = (co2) => {
-  let previous = 0; // Valeur seuil du niveau précédent (initialisé à 0)
-  for (let i = 0; i < levelThresholds.length; i++) {
-    if (co2 < levelThresholds[i].co2) {
-      // Si l'utilisateur est en dessous de ce palier
-      const currentThreshold = levelThresholds[i].co2; // Valeur du palier actuel
-      const percentage =
-        ((co2 - previous) / (currentThreshold - previous)) * 100; // Calcul du pourcentage de progression dans l'intervalle [previous, currentThreshold]
-      return Math.min(Math.max(percentage, 0), 100); // On limite le pourcentage entre 0 et 100
-    }
-    previous = levelThresholds[i].co2; // Sinon on passe au prochain palier, en mettant à jour la valeur de previous
-  }
-  return 100; // Si tous les paliers sont dépassés, on retourne 100% (niveau max atteint)
-};
-
 export default function HomeScreen({ navigation }) {
   const user = useSelector((state) => state.user.value); // Récupère l'utilisateur depuis Redux
-  const progress = useRef(new Animated.Value(0)).current; // Animation du niveau de progression
-
-  const totalSaved = user.totalSavedCo2; // total CO2 éonomisé par l'utilisateur
-  const userLevel = getUserLevel(totalSaved); // Niveau actuel
-  const levelIcon = levelIcons[userLevel]; // Icône correspondante
-  const nextLevelInfo = getNextLevelInfo(totalSaved); // Prochain niveau
-  const progressPercentage = getProgressPercentage(totalSaved); // Pourcentage progression
-
-  // À chaque changement de CO2 économisé, anime la barre de progression
-  useEffect(() => {
-    Animated.timing(progress, {
-      toValue: progressPercentage,
-      duration: 800,
-      useNativeDriver: false,
-    }).start();
-  }, [progressPercentage]);
-
-  // Contenu affiché si l'utilisateur est connecté
-  const co2Container = (
-    <>
-      {/* Infos CO2 + icône de niveau */}
-      <View style={styles.co2Section}>
-        <View style={styles.roundWrapper}>
-          <View style={styles.co2StatsWrapper}>
-            <View style={styles.co2ValueText}>
-              <Text style={{ fontSize: 26, fontWeight: "bold" }}>
-                {totalSaved}
-              </Text>
-              <Text style={{ fontSize: 20, fontWeight: "700" }}>kg</Text>
-            </View>
-            <Text style={styles.co2SavedTextInRound}>de CO2 économisés</Text>
-          </View>
-
-          {/* Icone plante */}
-
-          <Text style={styles.bigPlantLevelIcon}>{levelIcon}</Text>
-        </View>
-
-        {/* Texte explicatif */}
-
-        <View style={styles.descriptionContainer}>
-          <Text style={styles.description}>
-            Relever des défis, collecter du CO₂ et faire grandir sa plante à
-            chaque palier atteint !
-          </Text>
-        </View>
-      </View>
-
-      {/* Affichage du niveau + barre de progression */}
-
-      <View style={styles.levelLabelSection}>
-        <View style={{ flexDirection: "row", gap: 5 }}>
-          <Text style={styles.levelLabel}>
-            Niveau : {levelIcon} {userLevel}
-          </Text>
-        </View>
-
-        {/* Barre animée */}
-
-        <View style={styles.progressBarBackground}>
-          <Animated.View
-            style={[
-              styles.progressBarFill,
-              {
-                width: progress.interpolate({
-                  inputRange: [0, 100],
-                  outputRange: ["0%", "100%"],
-                }),
-              },
-            ]}
-          />
-        </View>
-
-        {/* Message pour prochain niveau */}
-        {nextLevelInfo && (
-          <Text style={styles.nextLevelLabel}>
-            Plus que {nextLevelInfo.remaining}kg pour atteindre{" "}
-            {nextLevelInfo.nextLevel} {nextLevelInfo.icon}
-          </Text>
-        )}
-      </View>
-
-      {/* Bouton vers l'historique */}
-
-      <View style={{ width: "90%" }}>
-        <CustomButton
-          title={"Historique de quêtes"}
-          onPress={() =>
-            navigation.navigate("User", { screen: "HistoryScreen" })
-          }
-        />
-      </View>
-    </>
-  );
 
   // Contenu si l'utilisateur n'est pas connecté
   const connectionContainer = (
@@ -258,7 +106,7 @@ export default function HomeScreen({ navigation }) {
       {/* Progression ou connexion selon état utilisateur */}
 
       <View style={styles.progressCard}>
-        {user.token ? co2Container : connectionContainer}
+        {user.token ? <HomeCo2Container user={user} /> : connectionContainer}
       </View>
     </SafeAreaView>
   );
@@ -325,93 +173,5 @@ const styles = StyleSheet.create({
     padding: 20,
     minHeight: 350,
     height: height * 0.55,
-  },
-
-  co2Section: {
-    alignItems: "center",
-    gap: 20,
-  },
-  // alignement horizontal des deux ronds (C02 + plante)
-  roundWrapper: {
-    flexDirection: "row",
-  },
-  co2StatsWrapper: {
-    borderColor: "#6AC46A",
-    borderWidth: 2,
-    borderRadius: 75,
-    aspectRatio: 1,
-    height: 130,
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 2,
-    padding: 20,
-  },
-
-  co2ValueText: {
-    flexDirection: "row",
-    alignItems: "flex-end",
-    gap: 3,
-  },
-  co2SavedTextInRound: {
-    color: "#565656",
-    fontSize: 14,
-    width: "90%",
-    textAlign: "center",
-  },
-
-  // icon de la grande plante à faire pousser (icon niveau actuel)
-  bigPlantLevelIcon: {
-    fontSize: 50,
-    textAlign: "center",
-    textAlignVertical: "center",
-    backgroundColor: "#DBF2D6",
-    paddingVertical: 15,
-    borderRadius: 150,
-    marginLeft: -15,
-    height: 130,
-    aspectRatio: 1,
-  },
-  // encars gris de description
-  description: {
-    fontSize: 13,
-    color: "#333333",
-    textAlign: "center",
-  },
-  descriptionContainer: {
-    backgroundColor: "#E0E0E0",
-    borderRadius: 10,
-    padding: 10,
-  },
-
-  // Niveau actuel
-  levelLabelSection: {
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 13,
-  },
-  levelLabel: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "#333333",
-    textAlign: "center",
-  },
-  nextLevelLabel: {
-    fontSize: 14,
-    color: "#565656",
-    textAlign: "center",
-  },
-  // BAR DE PROGRESSION DYNAMIQUE
-  progressBarBackground: {
-    width: 200,
-    height: 10,
-    backgroundColor: "#e0e0e0",
-    borderRadius: 10,
-    overflow: "hidden",
-    marginTop: 5,
-  },
-  progressBarFill: {
-    height: "100%",
-    backgroundColor: "#6AC46A",
-    borderRadius: 10,
   },
 });
